acda4812eb526a99ef2f61a29413a60d7f7837f3
Loïc.Lfc <loic.lefranc83@gmail.com>
2026-09-18 10:46:36 +0200

    Fix logo

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="src/css/style.css" />
     <!-- Bootstrap CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-    <link rel="shortcut icon" type="image/x-icon" href="src/images/article1.png" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 </head>
 <?php
 //load config
